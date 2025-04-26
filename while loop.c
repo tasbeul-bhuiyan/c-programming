af40309ca4d31355f650a87fd55bb02e4ab25c10
tasbeul-bhuiyan <tasbeulhoquebhuiyan@gmail.com>
2025-04-26 22:21:38 +0600

    Create while loop.c
    just an example of while loop

---
+++ while loop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+        int index=1;
+        while(index<=5){
+
+            printf("%d\n",index);
+            index++;    /* this line means index=index+1;*/
+
+
+
+}
+
+
+
+    return 0;
+}
